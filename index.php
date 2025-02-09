e00aeeec9ac44b34bcd96b84573b3459cb393fd9
TekGeek_dev <TekGeek_dev@protonmail.com>
2025-02-09 15:24:18 -0500

    modification header.php

--- index.php
+++ index.php
@@ -1,40 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>4W4-Voyage</title>
-    <link rel="stylesheet" href="normalize.css">
-    <link rel="stylesheet" href="assets/css/style.css">
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link
-        href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&family=Sriracha&display=swap"
-        rel="stylesheet">
-</head>
-
-<body class="wrapper">
-    <header class="header">
-        <img class="header__logo" src="assets/images/Club.png" alt="logo du site">
-        <input type="checkbox" class="header__check">
-        <div class="header__nav">
-
-            <nav class="nav">
-                <ul class="nav_list">
-                    <li class="nav__item"><a class="nav_link nav_link-active" href="">Aventure</a></li>
-                    <li class="nav__item"><a class="nav_link" href="">Sportive</a></li>
-                    <li class="nav__item"><a class="nav_link" href="">Culturelle</a></li>
-                    <li class="nav__item"><a class="nav_link" href="">Zen</a></li>
-                </ul>
-            </nav>
-            <form action="" class="header__search">
-                <input type="text" class="recherche__input">
-                <img src="https://s2.svgbox.net/hero-outline.svg?ic=search&color=000" width="20" height="20">
-            </form>
-        </div>
-
-    </header>
+<?php wp_head(); ?> 
     <main>
         <section class="hero full-bleed">
             <div class="hero__contenu">
