c8f46bfb6b07096486fe0825c51fd67a2dd78bce
TekGeek_dev <TekGeek_dev@protonmail.com>
2025-02-09 15:18:22 -0500

    modification function .php

--- functions.php
+++ functions.php
@@ -1,7 +1,8 @@
 <?php
 function theme_tp_enqueue_styles() { 
-wp_enqueue_style('normalize', get_template_directory_uri() . '/css/normalize.css'); 
-wp_enqueue_style('main-style', get_stylesheet_uri()); 
+    wp_enqueue_style('normalize', get_stylesheet_directory_uri() . '/normalize.css'); 
+    wp_enqueue_style('main-style', get_stylesheet_uri(), array('normalize'), filemtime(get_stylesheet_directory() . '/style.css')); 
 } 
 add_action('wp_enqueue_scripts', 'theme_tp_enqueue_styles');
+
 ?>
