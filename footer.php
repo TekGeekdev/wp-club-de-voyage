21f68cb12d17b4a95663aaa6191d70869a246d40
TekGeek_dev <TekGeek_dev@protonmail.com>
2025-02-20 23:14:59 -0500

    update footer.php

--- footer.php
+++ footer.php
@@ -1,9 +1,31 @@
-<footer class="footer">
-        <span class="footer_txt">© Par Mathieu Ledeur</span>
-        <div class="footer__social">
-            <img class="footer__social__img" src="assets/images/facebook.svg" alt="">
-            <img class="footer__social__img" src="assets/images/instagram.svg" alt="">
-            <img class="footer__social__img" src="assets/images/youtube.svg" alt="">
-        </div>
-    </footer>
-    <?php wp_footer(); ?>
+<footer id="nav-pied-page" class="plein-plan">
+            <div class="flex-avec-gap">
+                <a href="#">Acceuil</a>
+                <a href="#">Voyage</a>
+                <a href="#">Destination</a>
+            </div>
+            <form action="">
+                <input type="text" id="recherche" name="recherche" placeholder="faire une recherche"
+                    class="champ-recherche">
+                <input type="submit" value="Rechercher" class="bouton">
+            </form>
+            <div class="flex-avec-gap">
+                <a href="#">Acceuil</a>
+                <a href="#">Voyage</a>
+                <a href="#">Destination</a>
+            </div>
+            <div class="flex-avec-gap">
+                <p>3800 R. Sherbrooke E, Montréal, QC H1X 2A2</p>
+                <p>(514) 254-7131</p>
+                <a href="mailto:name@maisonneuve.com">Notre email</a>
+            </div>
+            <div class="boite-icones">
+                <img src="<?php echo get_template_directory_uri(); ?>/assets/images/facebook.png" alt="facebook">
+                <img src="<?php echo get_template_directory_uri(); ?>/assets/images/instagram.png" alt="instagram">
+                <img src="<?php echo get_template_directory_uri(); ?>/assets/images/twitter.png" alt="twitter">
+            </div>
+        </footer>
+
+</body>
+<?php wp_footer(); ?>
+</html>
--- front-page.php
+++ front-page.php
@@ -82,33 +82,5 @@
                 <img src="<?php echo get_template_directory_uri(); ?>/assets/images/port-grimaud.jpg" alt="">
             </article>
         </section>
-        <footer id="nav-pied-page" class="plein-plan">
-            <div class="flex-avec-gap">
-                <a href="#">Acceuil</a>
-                <a href="#">Voyage</a>
-                <a href="#">Destination</a>
-            </div>
-            <form action="">
-                <input type="text" id="recherche" name="recherche" placeholder="faire une recherche"
-                    class="champ-recherche">
-                <input type="submit" value="Rechercher" class="bouton">
-            </form>
-            <div class="flex-avec-gap">
-                <a href="#">Acceuil</a>
-                <a href="#">Voyage</a>
-                <a href="#">Destination</a>
-            </div>
-            <div class="flex-avec-gap">
-                <p>3800 R. Sherbrooke E, Montréal, QC H1X 2A2</p>
-                <p>(514) 254-7131</p>
-                <a href="mailto:name@maisonneuve.com">Notre email</a>
-            </div>
-            <div class="boite-icones">
-                <img src="<?php echo get_template_directory_uri(); ?>/assets/images/facebook.png" alt="facebook">
-                <img src="<?php echo get_template_directory_uri(); ?>/assets/images/instagram.png" alt="instagram">
-                <img src="<?php echo get_template_directory_uri(); ?>/assets/images/twitter.png" alt="twitter">
-            </div>
-        </footer>
 
-</body>
-</html>
+<?php get_footer(); ?>
