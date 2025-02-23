df3597b0638bc18e71dc6d928d82d9f3662da7cd
TekGeek_dev <TekGeek_dev@protonmail.com>
2025-02-23 16:41:18 -0500

    add search.php

--- header.php
+++ header.php
@@ -26,10 +26,12 @@
         'container' => false,
     ));
     ?>
-        </div>
-        <form role="search" method="get" action="<?php echo home_url('/'); ?>">
-            <input type="text" id="recherche" name="recherche" placeholder="<?php echo esc_attr_x('Rechercher des destinations...', 'placeholder') ?>" <?php echo esc_attr_x('Rechercher des destinations...', 'placeholder') ?>
-                class="champ-recherche">
-            <button type="submit" class="bouton"><?php echo esc_html_x('Search', 'submit button') ?></button>
-        </form>
+    </div>
+     <form role="search" method="get" class="search-form" action="<?php echo home_url('/'); ?>">
+        <label>
+            <span class="screen-reader-text"><?php echo _x('Search for:', 'label') ?></span>
+            <input type="search" class="search-field" placeholder="<?php echo esc_attr_x('Rechercher des destinations...', 'placeholder') ?>" value="<?php echo get_search_query() ?>" name="s" />
+        </label>
+        <button type="submit" class="search-submit"><?php echo esc_html_x('Search', 'submit button') ?></button>
+    </form>
     </nav>
