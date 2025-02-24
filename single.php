5452153b41c3332ed47f2ee2413d5ca025dbe6af
TekGeek_dev <TekGeek_dev@protonmail.com>
2025-02-24 09:33:38 -0500

    modification single.php

--- single.php
+++ single.php
@@ -1,41 +1,25 @@
 <?php get_header(); ?>
 
-<main class="contenu">
-    <?php
-    
-    if (have_posts()) :
-        while (have_posts()) : the_post(); ?>
-            <article class="destination">
-                <header>
-                    <?php if (has_post_thumbnail()) { ?>
-                        <img src="<?php echo get_the_post_thumbnail_url(); ?>" alt="<?php the_title(); ?>" class="image-mise-en-avant"> 
-                    <?php } ?>
-                    <h1><?php the_title(); ?></h1> 
-                </header>
-                <div class="description">
-                    <?php the_content(); ?> 
-                </div>
 
-                <?php
-                
-                if (have_rows('galerie_images')) : ?>
-                    <div class="galerie">
-                        <h2>Galerie d'images</h2>
-                        <div class="grille-galerie">
-                            <?php while (have_rows('galerie_images')) : the_row(); ?>
-                                <?php $image = get_sub_field('image'); ?>
-                                <div class="image-galerie">
-                                    <img src="<?php echo esc_url($image['url']); ?>" alt="<?php echo esc_attr($image['alt']); ?>">
-                                </div>
-                            <?php endwhile; ?>
-                        </div>
-                    </div>
-                <?php endif; ?>
-            </article>
-        <?php endwhile;
-    else : ?>
-        <p><?php _e('A ucune destination trouvée.', 'textdomain'); ?></p> 
-    <?php endif; ?>
-</main>
+    <main>
+        <section class="single">
+            <header>
+                <h2>Destinations populaires</h2>
+            </header>
+            <div class="grille-cartes">
+                <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+                <article>
+                        <?php if (has_post_thumbnail()) { ?>
+                            <img src="<?php echo get_the_post_thumbnail_url(); ?>" alt="<?php the_title(); ?>" class="image-mise-en-avant"> 
+                        <?php } ?>
+                    <h3><?php the_title(); ?></h3>
+                    <p><?php echo wp_trim_words(get_the_excerpt(), 25, " ... "); ?></p>
+                    <a href="<?php the_permalink(); ?>">Plus d'informations</a>
+                </article>
+                <?php endwhile; endif; ?>
+
+            </div>
+        </section>
+
 
 <?php get_footer(); ?>
