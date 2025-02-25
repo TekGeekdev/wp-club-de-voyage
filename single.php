eef4d209ae88d657b2d93c9dc89a3e826fbda4a7
TekGeek_dev <TekGeek_dev@protonmail.com>
2025-02-24 23:27:19 -0500

    add style single page

--- single.php
+++ single.php
@@ -3,21 +3,18 @@
 
     <main>
         <section class="single">
-            <header>
-                <h2>Destinations populaires</h2>
-            </header>
-            <div class="grille-cartes">
                 <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
                 <article>
-                        <?php if (has_post_thumbnail()) { ?>
-                            <img src="<?php echo get_the_post_thumbnail_url(); ?>" alt="<?php the_title(); ?>" class="image-mise-en-avant"> 
+                <?php if (has_post_thumbnail()) { ?>
+                            <img src="<?php echo get_the_post_thumbnail_url(); ?>" alt="<?php the_title(); ?>">
+                        <?php } else { ?>
+                            <img src="<?php echo get_template_directory_uri(); ?>/assets/images/port-grimaud.jpg" alt="image single article"> 
                         <?php } ?>
                     <h3><?php the_title(); ?></h3>
-                    <p><?php echo wp_trim_words(get_the_excerpt(), 25, " ... "); ?></p>
+                    <p><?php echo wp_strip_all_tags(get_the_content()); ?></p>
                     <a href="<?php the_permalink(); ?>">Plus d'informations</a>
                 </article>
                 <?php endwhile; endif; ?>
-            </div>
         </section>
     </main>
 
